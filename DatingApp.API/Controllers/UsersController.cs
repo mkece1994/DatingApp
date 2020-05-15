efae3d630415bda60980256404c999aedc5f7da9
Mahesh Kumar Patel <mkece1994@gmail.com>
2020-05-16 03:32:04 +0530

    initial commit

---
+++ DatingApp.API/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            Console.WriteLine("userdfbghjmahehs");
+            var users = await _context.Users.ToListAsync();
+            Console.WriteLine(users);
+            return Ok(users);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.id == id);
+
+            return Ok(user);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string user)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string  user)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DatingApp.API/Models/User.cs
@@ -0,0 +1,8 @@
+namespace DatingApp.API.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
